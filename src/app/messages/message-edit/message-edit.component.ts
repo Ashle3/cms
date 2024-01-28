34329838845129385cacb8e4c2dbd8c7a224c9d8
Ashle3 <ashleeb3@byui.edu>
2024-01-27 22:57:40 -0600

    finished w3 assignment

--- src/app/messages/message-edit/message-edit.component.ts
+++ src/app/messages/message-edit/message-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'app-message-edit',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './message-edit.component.css'
 })
 export class MessageEditComponent {
+  @Output() addMessageEvent = new EventEmitter<Message>();
+  @ViewChild('subject') subject: ElementRef;
+  @ViewChild('msgText') msgText: ElementRef;
+ 
+  currentSender: string = 'Ashlee Butterfield'; 
 
+  onSendMessage() {
+    const subject = this.subject.nativeElement.value;
+    const msgText = this.msgText.nativeElement.value;
+    const newMessage = new Message('1', subject, msgText, this.currentSender);
+    this.addMessageEvent.emit(newMessage);
+  }
+
+  onClear() {
+    this.subject.nativeElement.value = " ";
+    this.msgText.nativeElement.value = " ";
+  }
 }
--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'app-message-list',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './message-list.component.css'
 })
 export class MessageListComponent {
+  messages: Message[] = [
+    new Message('2', 'Grades', 'When will the grades for this assignment be posted?', 'Steve'),
+    new Message('3', 'Grades Posted', 'The grades for this assignment will be posted tomorrow at 8am.', 'Bro. Jackson'),
+    new Message('4', 'Thanks!', 'Thank you Brother Jackson!', 'Steve')
+  ]
 
+  onAddMessage(message: Message){
+    this.messages.push(message);
+  }
 }
