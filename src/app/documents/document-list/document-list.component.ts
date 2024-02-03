66df502d62e484f0d26014f35d9c0b2f1bed6bfb
Ashle3 <ashleeb3@byui.edu>
2024-02-02 21:19:49 -0600

    fixed the birthdays spelling

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -12,7 +12,7 @@ export class DocumentListComponent {
   documents: Document[] = [
     new Document('1', 'Address Book', 'Contains the addresses of people I know', 'www.addressbook.com'),
     new Document('2', 'Doctor Appointments', 'Contains the doctor appointments I have scheduled in the next month', 'www.doctorappointment.com'),
-    new Document('3', 'Birtdays', 'Contains a list of birthdays of the people I know', 'www.birthdays.com'),
+    new Document('3', 'Birthdays', 'Contains a list of birthdays of the people I know', 'www.birthdays.com'),
     new Document('4', 'Reminders', 'Holds a list of reminders I have need for the next 6 months', 'www.reminders.com')
   ];
 
