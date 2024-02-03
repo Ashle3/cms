6e63b3b46c5e1cb1732529a21198b00bea815989
Ashle3 <ashleeb3@byui.edu>
2024-02-02 20:35:42 -0600

    document-list component completed

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-list',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './document-list.component.css'
 })
 export class DocumentListComponent {
+  @Output() selectedDocumentEvent = new EventEmitter();
 
+  documents: Document[] = [
+    new Document('1', 'Address Book', 'Contains the addresses of people I know', '-----'),
+    new Document('2', 'Doctor Appointments', 'Contains the doctor appointments I have scheduled in the next month', '----'),
+    new Document('3', 'Birtdays', 'Contains a list of birthdays of the people I know', 'a;sldf;'),
+    new Document('4', 'Reminders', 'Holds a list of reminders I have need for the next 6 months', 'asf;lj')
+  ];
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 }
