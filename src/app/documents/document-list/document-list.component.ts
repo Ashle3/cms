b6ef83eaf0ac86eda07b0f0c5f53fea8fb268cdb
Ashle3 <ashleeb3@byui.edu>
2024-02-10 23:09:56 -0600

    finished the document services

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,22 +1,22 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
 
 @Component({
   selector: 'app-document-list',
   templateUrl: './document-list.component.html',
   styleUrl: './document-list.component.css'
 })
-export class DocumentListComponent {
-  @Output() selectedDocumentEvent = new EventEmitter();
+export class DocumentListComponent implements OnInit{
+  documents: Document[] = [];
+  
+  constructor(private documentService: DocumentService){};
 
-  documents: Document[] = [
-    new Document('1', 'Address Book', 'Contains the addresses of people I know', 'www.addressbook.com'),
-    new Document('2', 'Doctor Appointments', 'Contains the doctor appointments I have scheduled in the next month', 'www.doctorappointment.com'),
-    new Document('3', 'Birthdays', 'Contains a list of birthdays of the people I know', 'www.birthdays.com'),
-    new Document('4', 'Reminders', 'Holds a list of reminders I have need for the next 6 months', 'www.reminders.com')
-  ];
+  ngOnInit(): void {
+      this.documents = this.documentService.getDocuments();
+  }
 
   onSelectedDocument(document: Document) {
-    this.selectedDocumentEvent.emit(document);
+    this.documentService.documentSelectedEvent.emit(document);
   }
 }
