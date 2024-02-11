a1ca43e988eecfaeefb0a241e506e84ce16d794c
Ashle3 <ashleeb3@byui.edu>
2024-02-10 23:31:18 -0600

    got mock messages to appear

--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Message } from '../message.model';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-message-list',
   templateUrl: './message-list.component.html',
   styleUrl: './message-list.component.css'
 })
-export class MessageListComponent {
-  messages: Message[] = [
-    new Message('2', 'Grades', 'When will the grades for this assignment be posted?', 'Steve'),
-    new Message('3', 'Grades Posted', 'The grades for this assignment will be posted tomorrow at 8am.', 'Bro. Jackson'),
-    new Message('4', 'Thanks!', 'Thank you Brother Jackson!', 'Steve')
-  ]
+export class MessageListComponent implements OnInit{
+  messages: Message[] = [];
+
+  constructor(private messageService: MessageService){};
+
+  ngOnInit(): void {
+      this.messages = this.messageService.getMessages();
+  }
 
   onAddMessage(message: Message){
     this.messages.push(message);
