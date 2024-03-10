6e3228e375230c0db47b0aeaa2748696e3db1d17
Ashle3 <ashleeb3@byui.edu>
2024-03-10 01:52:28 -0600

    finished with w09

--- src/app/messages/message.service.ts
+++ src/app/messages/message.service.ts
@@ -1,4 +1,6 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { Message } from './message.model';
 import { MOCKMESSAGES } from './MOCKMESSAGES';
 
@@ -8,12 +10,29 @@ import { MOCKMESSAGES } from './MOCKMESSAGES';
 export class MessageService {
   messageChangedEvent = new EventEmitter<Message[]>();
   private messages: Message[] = [];
+  maxMessageId: number;
 
-  constructor() { 
+  constructor(private http: HttpClient) { 
     this.messages = MOCKMESSAGES;
   }
 
+  compareNumbers(a, b){
+    return a-b;
+  }
+
   getMessages(): Message[]{
+    this.http
+      .get('https://ab-cms-5ee91-default-rtdb.firebaseio.com/messages.json')
+      .subscribe(
+        //success
+        (messages: Message[])=>{
+          this.messages = messages;
+          this.maxMessageId = this.getMaxId();
+          this.messages.sort(this.compareNumbers);
+          let messagesListClone = this.messages.slice();
+          this.messageChangedEvent.next(messagesListClone);
+        }
+      );
     return this.messages.slice();
   }
   
@@ -23,6 +42,32 @@ export class MessageService {
 
   addMessage(message: Message){
     this.messages.push(message);
-    this.messageChangedEvent.emit(this.messages.slice());
+    // this.messageChangedEvent.emit(this.messages.slice());
+    this.storeMessages();
   }
+
+  getMaxId(): number{
+    let maxId = 0;
+
+    for(let message of this.messages){
+      let currentId = +message.id;
+      if (currentId > maxId){
+        maxId = currentId;
+      }
+    }
+
+    return maxId;
+  }
+
+  storeMessages() {
+    let arrayToText = JSON.parse(JSON.stringify(this.messages));
+    this.http.put('https://ab-cms-5ee91-default-rtdb.firebaseio.com/messages.json', arrayToText, {headers: new HttpHeaders({'Content-Type': 'application/json'})})
+      .subscribe(
+        () => {
+          let messagesListClone = this.messages.slice();
+          this.messageChangedEvent.next(messagesListClone);
+        }
+      )
+  }
+  
 }
